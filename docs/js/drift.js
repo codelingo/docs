aecc9d16a4082d9a448638e0366d849eaf5bfbc9
rstorr <reuben@codelingo.io>
2019-02-27 09:20:53 +1300

    update chatbot

--- docs/js/drift.js
+++ docs/js/drift.js
@@ -1,62 +1,24 @@
 "use strict";
 
-!function() {
+!function () {
   var t = window.driftt = window.drift = window.driftt || [];
   if (!t.init) {
     if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-    t.factory = function(e) {
-      return function() {
-        var n = Array.prototype.slice.call(arguments);
-        return n.unshift(e), t.push(n), t;
+    t.invoked = !0, t.methods = ["identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on"],
+      t.factory = function (e) {
+        return function () {
+          var n = Array.prototype.slice.call(arguments);
+          return n.unshift(e), t.push(n), t;
+        };
+      }, t.methods.forEach(function (e) {
+        t[e] = t.factory(e);
+      }), t.load = function (t) {
+        var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+        o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+        var i = document.getElementsByTagName("script")[0];
+        i.parentNode.insertBefore(o, i);
       };
-    }, t.methods.forEach(function(e) {
-      t[e] = t.factory(e);
-    }), t.load = function(t) {
-      var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-      o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-      var i = document.getElementsByTagName("script")[0];
-      i.parentNode.insertBefore(o, i);
-    };
   }
 }();
 drift.SNIPPET_VERSION = '0.3.1';
-drift.load('znt7yw7xwvs3');
-
-(function() {
-var DRIFT_CHAT_SELECTOR = '.drift-open-chat'
-  /* http://youmightnotneedjquery.com/#ready */
-  function ready(fn) {
-    if (document.readyState != 'loading') {
-      fn();
-    } else if (document.addEventListener) {
-      document.addEventListener('DOMContentLoaded', fn);
-    } else {
-      document.attachEvent('onreadystatechange', function() {
-        if (document.readyState != 'loading')
-          fn();
-      });
-    }
-  }
-  /* http://youmightnotneedjquery.com/#each */
-  function forEachElement(selector, fn) {
-    var elements = document.querySelectorAll(selector);
-    for (var i = 0; i < elements.length; i++)
-      fn(elements[i], i);
-  }
-  function openSidebar(driftApi, event) {
-    debugger
-    event.preventDefault();
-    driftApi.sidebar.open();
-    return false;
-  }
-  ready(function() {
-    drift.on('ready', function(api) {
-      var handleClick = openSidebar.bind(this, api)
-      forEachElement(DRIFT_CHAT_SELECTOR, function(el) {
-        el.addEventListener('click', handleClick);
-      });
-    });
-  });
-})()
-
+drift.load('g9kff5rmy54c');
