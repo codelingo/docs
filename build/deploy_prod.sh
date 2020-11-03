0fad9fc3c03dac0bde0587f451d35f78311fc7df
rstorr <reuben@codelingo.io>
2020-11-03 18:35:32 +1300

    for deploy

--- build/deploy_prod.sh
+++ build/deploy_prod.sh
@@ -4,6 +4,6 @@ set -e
 
 codeship_google authenticate
 
-gcloud container clusters get-credentials production --region us-west1
+gcloud container clusters get-credentials production --region us-west2-b
 
 helm upgrade docs build/chart -f build/chart/prod-values.yaml --wait
--- build/deploy_staging.sh
+++ build/deploy_staging.sh
@@ -4,6 +4,6 @@ set -e
 
 codeship_google authenticate
 
-gcloud container clusters get-credentials staging --region us-west1
+gcloud container clusters get-credentials staging --region us-west2-a
 
 helm upgrade docs build/chart -f build/chart/staging-values.yaml --wait
